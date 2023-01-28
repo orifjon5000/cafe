e761b25505ebd2e53349fad82b4c0a904abe7b0a
orifjon <orifjon.5000@gmail.com>
2023-01-28 15:04:39 +0900

    fix: modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ npm i yarn -g
 yarn global add serve
 yarn
 # yarn run build
-pm2 start "yarn run start:prod" --name=Fruit-REACT
+pm2 serve build 4000  --spa --name=Cafe
 
 # DEVELOPMENT
 # npm i yarn -g
